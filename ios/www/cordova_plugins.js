10da429d96e4a790ec5a8ed4da7421f5f2377b4b
sirius2013 <betopone310@gmail.com>
2014-08-14 04:09:41 +0800

    first commit
    first commit

---
+++ ios/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.appgiraffe.plugins.applicationPreferences/www/applicationPreferences.js",
+        "id": "com.appgiraffe.plugins.applicationPreferences.applicationPreferences",
+        "clobbers": [
+            "appgiraffe.plugins.applicationPreferences"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.appgiraffe.plugins.applicationPreferences": "0.1.0"
+}
+// BOTTOM OF METADATA
+});
